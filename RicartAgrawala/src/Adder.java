4604e1cdb27dc231a95905d61c4e3eeca35f1f43
erilyth <vishal.batchu@students.iiit.ac.in>
2016-09-15 16:53:23 +0530

    Add basic remote methods

--- RicartAgrawala/src/Adder.java
+++ RicartAgrawala/src/Adder.java
@@ -2,5 +2,17 @@ import java.rmi.*;
 
 public interface Adder extends Remote{ 
 	public int add(int x,int y)
-			throws RemoteException;  
+			throws RemoteException;
+	public int ReceiveLockRequest(String sender, int clock)
+			throws RemoteException;
+	public int LockRequest(String receiver)
+			throws RemoteException;
+	public int SendAckRequest(String receiver)
+			throws RemoteException;
+	public void ReceiveAckRequest(String sender)
+			throws RemoteException;
+	public int UnlockRequest(String receiver, int clock)
+			throws RemoteException;
+	public void ReceiveUnlockRequest(String receiver)
+			throws RemoteException;
 }
